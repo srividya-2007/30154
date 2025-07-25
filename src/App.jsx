21c623a0c65063c44483f923e4a04478c314ece1
srividya-2007 <2400030154@kluniversity.in>
2025-07-25 09:57:16 +0530

    Success Message

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+      <>
+       
+        <html lang="en">
+        <head>
+          <title>My First Webpage</title>
+        </head>
+        <body>
+          <h1>Lanka Srividya</h1>
+          <h2>Frontend Developer</h2>
+          <p>Welcome to my project. This is my first Git commit in a new development environment using VS Code.</p>
+        </body>
+        </html>
+
+        </>
+        
+  )
+}
+export default App
+
+
+
